29d9300e38c36673d578fffb7733deea617d9031
Domen Oslaj <doslaj@gmail.com>
2020-09-25 22:46:41 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = input("What is your mood today? ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Oh no :(")
+elif mood == "excited":
+    print("Great to see you excited!")
+elif mood == "relaxed":
+    print("Yoga bro? :)")
+else:
+    print("I don't recognize this mood.")
